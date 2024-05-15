3a384ee11f77dd030aec7410216df793e58fb9d3
Mattia Ducoli <121670304+mducoli@users.noreply.github.com>
2024-05-15 10:10:20 +0200

    devshell + flake update

--- flake.nix
+++ flake.nix
@@ -5,15 +5,31 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: rec {
+  outputs = {nixpkgs, ...}: let
+    lib = nixpkgs.lib;
+    systems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+      "aarch64-linux"
+    ];
+    forEachSystem = f: lib.genAttrs systems (system: f pkgsFor.${system});
+    pkgsFor = lib.genAttrs systems (system:
+      import nixpkgs {
+        inherit system;
+      });
+  in rec {
     homeManagerModules = rec {
       kawari = import ./module;
       default = kawari;
     };
 
     homeManagerModule = homeManagerModules.kawari;
+
+    devShells = forEachSystem (pkgs: {
+      default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [go gopls];
+      };
+    });
   };
 }
